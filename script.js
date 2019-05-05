f1e4b1d96f5dcc52007719526fc04b93ad41373f
SpInAgRblzzz <36811343+SpInAgRblzzz@users.noreply.github.com>
2019-05-05 18:23:50 +0300

    Найдены элементы навигации

---
+++ script.js
@@ -0,0 +1,3 @@
+const fillButton = document.querySelector('.fill-matrix');
+const resetButton = document.querySelector('.reset-matrix');
+const switchMode = document.querySelector('.mode-switcher');
