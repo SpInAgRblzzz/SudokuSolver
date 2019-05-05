f44fe16d63f2b6dea6d764c11b9cdb638973a3fb
SpInAgRblzzz <36811343+SpInAgRblzzz@users.noreply.github.com>
2019-05-05 18:35:06 +0300

    Функция получения матрицы из input

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 const fillButton = document.querySelector('.fill-matrix');
 const resetButton = document.querySelector('.reset-matrix');
 const switchMode = document.querySelector('.mode-switcher');
+
+function getMatrix(){
+    let matrix = [];
+    for(let row = 0; row < 9; row++){
+        let matrixRow = [];
+        for(let column = 0; column < 9; column++){
+            matrixRow.push(+document.querySelector(`.row-${row}.column-${column}`).value);
+        }
+        matrix.push(matrixRow);
+    }    
+    return matrix;
+}
+
+    
